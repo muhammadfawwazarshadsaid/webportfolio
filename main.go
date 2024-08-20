9f3d515c2bdffacb763a8d8cd3018550b824313e
muhammadfawwazarshadsaid <muhammad.fawwaz22@ui.ac.id>
2024-08-20 20:09:36 +0700

    ubah port

--- main.go
+++ main.go
@@ -2,33 +2,57 @@ package main
 
 import (
 	"log"
-	"webportfolio/config"
+	// "webportfolio/config"
 	"webportfolio/database"
 	"webportfolio/database/migration"
 	route "webportfolio/routers"
 
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/logger"
-	"github.com/gofiber/template/html/v2"
+
+	// "github.com/gofiber/template/html/v2"
 	_ "github.com/lib/pq"
 )
 
+// func main() {
+// 	// Initialize the HTML template engine
+// 	engine := html.New("./public/views", ".html")
+
+// 	app := fiber.New(fiber.Config{
+// 		Views: engine,
+// 	})
+
+// 	// Middleware for logging requests
+// 	app.Use(logger.New(logger.Config{
+// 		Format: "${time} ${ip} ${method} ${path} ${status} ${latency}\n",
+// 	}))
+
+// 	// Serve static files from "public" directory
+// 	app.Static("/", config.StaticDir)
+
+// 	// Database and migration
+// 	database.ConnectDB()
+// 	migration.Migration()
+
+// 	// Routes
+// 	route.RouterApp(app)
+
+// 	// Start server
+// 	port := ":8080"
+// 	err := app.Listen(port)
+// 	if err != nil {
+// 		log.Fatal(err)
+// 	}
+// }
+
 
 func main() {
-	// Initialize the HTML template engine
-	engine := html.New("./public/views", ".html")
-
-	app := fiber.New(fiber.Config{
-		Views: engine,
-	})
+	app := fiber.New()
 
 	// Middleware for logging requests
 	app.Use(logger.New(logger.Config{
 		Format: "${time} ${ip} ${method} ${path} ${status} ${latency}\n",
 	}))
-
-	// Serve static files from "public" directory
-	app.Static("/", config.StaticDir)
 
 	// Database and migration
 	database.ConnectDB()
@@ -38,7 +62,7 @@ func main() {
 	route.RouterApp(app)
 
 	// Start server
-	port := ":8080"
+	port := ":80"
 	err := app.Listen(port)
 	if err != nil {
 		log.Fatal(err)
